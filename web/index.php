4a1f67311e522543c7774998272cdd2b50b1194f
Lucas A. Batista <lucasti-cba@outlook.com>
2022-03-05 21:01:07 -0400

    7

--- web/index.php
+++ web/index.php
@@ -2,9 +2,18 @@
 
 require('../vendor/autoload.php');
 
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
 
 $app['debug'] = true;
 
 $app['twig']->render('index.twig');
 
 $app->run();
+
